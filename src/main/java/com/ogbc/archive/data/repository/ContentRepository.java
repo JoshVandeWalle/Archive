8fbc0f5c0fa3e2f5df951dd1b0c34c600ed07fcc
JoshVandeWalle <joshv98@icloud.com>
2022-07-16 17:37:35 -0700

    Improve passage searches

--- src/main/java/com/ogbc/archive/data/repository/ContentRepository.java
+++ src/main/java/com/ogbc/archive/data/repository/ContentRepository.java
@@ -10,15 +10,30 @@ import java.util.List;
 public interface ContentRepository extends JpaRepository<ContentEntity, Long>
 {
     @Query(value="SELECT DISTINCT CONTENT.* FROM CONTENT JOIN CONTENT_TOPIC ON CONTENT.id = CONTENT_TOPIC.content_id JOIN TOPIC ON CONTENT_TOPIC.topic_id = TOPIC.id where TOPIC.topic LIKE %:name% ORDER BY CONTENT.`date`;", nativeQuery = true)
-    List<ContentEntity> findByTopic(@Param("name") String name);
+    List<ContentEntity> findByTopic(String name);
     @Query(value= """
             SELECT * from CONTENT WHERE passage LIKE CONCAT('%', :book, '%') AND\s
-            (passage LIKE CONCAT('%', :chapter, ':%') OR (LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) > 1 AND\s
+            (passage LIKE CONCAT('%', :chapter, ':%') OR
+            (passage NOT LIKE ':' AND passage LIKE CONCAT('%', :chapter, '%')) OR
+            (LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) > 1 AND\s
             CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 1), ' ', -1) AS UNSIGNED) <= :chapter AND\s
             CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 2), '-', -1) AS UNSIGNED) >= :chapter));""", nativeQuery = true)
-    List<ContentEntity> findByChapter(@Param("book") String book, String chapter);
-    @Query(value="SELECT * FROM CONTENT WHERE passage LIKE %:book% AND passage LIKE CONCAT('%', :chapter, ':%') AND passage LIKE CONCAT ('%:%', :verse, '%')", nativeQuery = true)
-    List<ContentEntity> findByVerse(String book, String chapter, String verse);
+    List<ContentEntity> findByChapter(@Param("book") String book, @Param("chapter") String chapter);
+    @Query(value= """
+            SELECT * from CONTENT WHERE passage LIKE CONCAT('%', :book, '%') AND
+            ((passage NOT LIKE '%:%' AND SUBSTRING_INDEX(passage, ' ', -1) = :chapter) OR
+            (LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) = 1 AND passage LIKE '%:%' AND passage NOT LIKE '%-%' AND SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 1), ' ', -1) = :chapter\s
+            \tAND SUBSTRING_INDEX(passage, ':', -1) = :verse) OR
+            LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) = 1 AND passage LIKE '%-%' AND SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 1), ' ', -1) = :chapter AND
+            \tSUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', -1), '-', 1) <= :verse AND
+            \tSUBSTRING_INDEX(passage, '-', -1) >= :verse OR
+            LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) = 2 AND SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 1), ' ', -1) < :chapter AND
+            \tSUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 2), '-', -1) > :chapter OR
+            LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) = 2 AND SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 1), ' ', -1) = :chapter AND
+            \tSUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', -2), '-', 1) <= :verse OR
+            LENGTH(passage) - LENGTH(REPLACE(passage, ':', '')) = 2 AND SUBSTRING_INDEX(SUBSTRING_INDEX(passage, ':', 2), '-', -1) = :chapter AND
+            \tSUBSTRING_INDEX(passage, ':', -1) >= :verse)""", nativeQuery = true)
+    List<ContentEntity> findByVerse(@Param("book") String book, @Param("chapter") String chapter,  @Param("verse") String verse);
     @Query(value = "SELECT * FROM CONTENT ORDER BY date LIMIT 10", nativeQuery = true)
     List<ContentEntity> findRecent();
 }

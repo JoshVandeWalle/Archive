c4398d51dc0515cb07bb5946dac1676b0e98a2cb
JoshVandeWalle <joshv98@icloud.com>
2022-09-27 19:49:21 -0700

    Add root URL for health check

---
+++ src/main/java/com/ogbc/archive/api/HealthRestService.java
@@ -0,0 +1,18 @@
+package com.ogbc.archive.api;
+
+import com.ogbc.archive.api.dto.RestDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthRestService
+{
+    @GetMapping("/")
+    public ResponseEntity<?> handleHealthCheck()
+    {
+        return new ResponseEntity<RestDto>(new RestDto<String>(null, "UP"), HttpStatus.OK);
+    }
+}

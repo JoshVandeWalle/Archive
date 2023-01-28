8f3360fbc17209e32ebb8a24c23f6ad5f4fe49d4
JoshVandeWalle <joshv98@icloud.com>
2023-01-28 11:24:05 -0800

    Read config from .yml following DRY

--- src/main/java/com/ogbc/archive/config/security/JwtConfig.java
+++ src/main/java/com/ogbc/archive/config/security/JwtConfig.java
@@ -2,26 +2,28 @@ package com.ogbc.archive.config.security;
 
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
-@PropertySource(value={"classpath:application.yml"})
 public class JwtConfig
 {
-    //@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    //private String jwkSetUri;
+    @Autowired
+    private Environment environment;
 
     @Bean
     public JwtDecoder jwtDecoder()
     {
-        return NimbusJwtDecoder.withJwkSetUri("https://api.asgardeo.io/t/jvdw/oauth2/jwks")
+        return NimbusJwtDecoder.withJwkSetUri(environment.getProperty("spring.security.oauth2.resourceserver.jwt.jwk-set-uri"))
                 .jwtProcessorCustomizer(customizer -> {
                     customizer.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")));
                 })

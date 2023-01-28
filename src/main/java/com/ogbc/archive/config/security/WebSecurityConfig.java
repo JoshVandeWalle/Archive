90a3b579d0f31a7d32bed4247fc0576a24b1b05a
JoshVandeWalle <joshv98@icloud.com>
2023-01-28 10:53:49 -0800

    Adding authorization via Asgardeo

---
+++ src/main/java/com/ogbc/archive/config/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ogbc.archive.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig
+{
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(configurer ->
+                        configurer
+                                .antMatchers("/", "/content", "/content/topic/**", "/content/passage/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                // Enable JWT Authentication
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .build();
+    }
+}

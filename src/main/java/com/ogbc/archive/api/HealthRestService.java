72c4ef02b669f5624a96d7590e89c0cc02e02cb1
JoshVandeWalle <joshv98@icloud.com>
2023-02-08 18:00:15 -0800

    Remove unused imports

--- src/main/java/com/ogbc/archive/api/HealthRestService.java
+++ src/main/java/com/ogbc/archive/api/HealthRestService.java
@@ -2,12 +2,6 @@ package com.ogbc.archive.api;
 
 import com.ogbc.archive.api.dto.RestDto;
 import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;

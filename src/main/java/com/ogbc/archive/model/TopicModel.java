7febb5f906a28a7e9858c2ce0f6ddf9e46856272
JoshVandeWalle <joshv98@icloud.com>
2023-07-25 18:04:23 -0700

    Remove unused imports from TopicModel

--- src/main/java/com/ogbc/archive/model/TopicModel.java
+++ src/main/java/com/ogbc/archive/model/TopicModel.java
@@ -1,13 +1,10 @@
 package com.ogbc.archive.model;
 
-import com.ogbc.archive.data.entity.ContentEntity;
 import com.ogbc.archive.data.entity.TopicEntity;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.validation.constraints.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor

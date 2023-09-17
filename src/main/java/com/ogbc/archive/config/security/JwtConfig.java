46b34065c8f81c7ad42561ea68508b260aa39f74
JoshVandeWalle <joshv98@icloud.com>
2023-09-17 16:32:05 -0700

    Remove unused import

--- src/main/java/com/ogbc/archive/config/security/JwtConfig.java
+++ src/main/java/com/ogbc/archive/config/security/JwtConfig.java
@@ -5,7 +5,6 @@ import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;

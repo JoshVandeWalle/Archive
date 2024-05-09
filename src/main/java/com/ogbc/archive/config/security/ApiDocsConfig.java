7e19f8785b780f85070886a161e4554e1b0b1377
JoshVandeWalle <joshv98@icloud.com>
2024-05-08 21:15:18 -0700

    Add server

--- src/main/java/com/ogbc/archive/config/security/ApiDocsConfig.java
+++ src/main/java/com/ogbc/archive/config/security/ApiDocsConfig.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "OGBC Content API", version = "v0"))
+@OpenAPIDefinition(info = @Info(title = "OGBC Content API", version = "v1"), servers = {@Server(url = "/", description = "Default Server URL")})
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
